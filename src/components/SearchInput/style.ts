c37df49e514119e7e56c612c5683b4f527b9a1e2
Gabriel Guimaraes <silva.guimaraes@ifood.com.br>
2023-04-01 15:33:48 -0300

    styling input

--- src/components/SearchInput/style.ts
+++ src/components/SearchInput/style.ts
@@ -4,11 +4,15 @@ import { styled } from "@/styles";
 export const Input = styled('input', {
     height: 40,
     width: 400,
-    padding: 5,
+    padding: 8,
     fontSize: 24,
-    border: 'solid "$primary" 2px',
+    border: 'none',
+    borderRadius: 8,
+    backgroundColor: "$primary",
+    color: 'white',
 
     '&:focus': {
         outline: "transparent",
+        border: 'solid 1px white'
     }
 })
